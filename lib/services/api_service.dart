853f3b84d60a3fc586606e36bd71a1769cb71071
zouhour bouchhiwa <zouhourbosh@gmail.com>
2025-12-05 22:55:12 +0100

    Add Flutter web build for Vercel

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,7 +7,7 @@ import '../config/app_config.dart';
 
 class ApiService {
   // Use configurable base URL from app config
-  static String get baseUrl => AppConfig.apiBaseUrl;
+  static String get baseUrl => 'https://skillbridge-backend.onrender.com/api';
   final storage = const FlutterSecureStorage();
 
   Future<String?> getToken() async {
