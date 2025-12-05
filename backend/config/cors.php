af64b0f6409a650039e25d072630e37cc3cd2172
zouhour bouchhiwa <zouhourbosh@gmail.com>
2025-12-05 23:25:57 +0100

    Fix syntax error in cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,27 @@
-<? php
+<?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    
+
     'allowed_methods' => ['*'],
-    
-    'allowed_origins' => ['*'],  // Temporairement autorise TOUS les domaines
-    
+
+    'allowed_origins' => ['*'],
+
     'allowed_origins_patterns' => [],
-    
+
     'allowed_headers' => ['*'],
-    
+
     'exposed_headers' => [],
-    
+
     'max_age' => 0,
-    
-    'supports_credentials' => false,  // Important : mettre false quand allowed_origins = *
+
+    'supports_credentials' => false,
+
 ];
